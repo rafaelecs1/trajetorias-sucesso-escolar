9a941d35e6a64a8767e664bc0be5aa16c04f9edf
Sandy <ssantos@unicef.org>
2019-10-10 11:34:01 -0300

    instituto claro footer

--- wp-content/themes/trajetoria_escolar/footer.php
+++ wp-content/themes/trajetoria_escolar/footer.php
@@ -10,9 +10,9 @@
  */
 
 ?>
-	</div><!-- #content -->
-    <?php
-    if (is_front_page()) {
+</div><!-- #content -->
+<?php
+if (is_front_page()) {
     ?>
     <section id="faixa-bottom">
         <div class="center">
@@ -21,35 +21,35 @@
         </div>
     </section>
     <?php
-    }
-    ?>
-	<footer id="colophon" class="site-footer">
-		<div class="center">
-            <ul class="realizacao">
-                <?php
-                $realizacao = array(
-                    'unicef' => array('url' => 'https://www.unicef.org/brazil/pt/', 'tipo' => 'Iniciativa'),
-//                    'samsung' => array('url' => 'https://www.samsung.com/br/', 'tipo' => 'Parcerias Estratégicas'),
-                    'netclaroembratel' => array('url' => 'https://www.institutonetclaroembratel.org.br/', 'tipo' => 'Parceiro Estratégico'),
-                    'aprendiz' => array('url' => 'http://www.cidadeescolaaprendiz.org.br/', 'tipo' => 'Parceiro técnico'),
-                    );
-                foreach($realizacao as $k => $v)
-                {
-                    echo '<li><span>', $v['tipo'], '</span><a href="', $v['url'], '" class="realizador ', $k, '" target="_blank"><img src="', get_template_directory_uri(), '/img/logo/', $k, '.png" alt="', strtoupper($k), '" title="', strtoupper($k), '"/></a></li>';
-                }
-                ?>
-            </ul>
-            <ul class="share">
-                <?php
-                global $arShare;
-                foreach($arShare as $k =>$v)
-                {
-                    echo '<li><a class="', $k,'" href="', $v, '" title="Compartilhe no ', ucfirst($k), '"><img src="', get_template_directory_uri(), '/img/', $k, '-share-footer.png" alt="', strtoupper($k), '"/></a></li>';
-                }
-                ?>
-            </ul>
-        </div>
-	</footer><!-- #colophon -->
+}
+?>
+<footer id="colophon" class="site-footer">
+    <div class="center">
+        <ul class="realizacao">
+            <?php
+            $realizacao = array(
+                'unicef' => array('url' => 'https://www.unicef.org/brazil/pt/', 'tipo' => 'Realização'),
+                'netclaroembratel' => array('url' => 'https://www.institutonetclaroembratel.org.br/', 'tipo' => 'Parceiro Estratégico'),
+                'itau' => array('url' => 'https://www.samsung.com/br/', 'tipo' => 'Apoio'),
+                'aprendiz' => array('url' => 'http://www.cidadeescolaaprendiz.org.br/', 'tipo' => 'Parceiro técnico'),
+                'cedac' => array('url' => 'https://www.samsung.com/br/', 'tipo' => ''),
+
+            );
+            foreach ($realizacao as $k => $v) {
+                echo '<li><span>', $v['tipo'], '</span><a href="', $v['url'], '" class="realizador ', $k, '" target="_blank"><img src="', get_template_directory_uri(), '/img/logo/', $k, '.png" alt="', strtoupper($k), '" title="', strtoupper($k), '"/></a></li>';
+            }
+            ?>
+        </ul>
+        <ul class="share">
+            <?php
+            global $arShare;
+            foreach ($arShare as $k => $v) {
+                echo '<li><a class="', $k, '" href="', $v, '" title="Compartilhe no ', ucfirst($k), '"><img src="', get_template_directory_uri(), '/img/', $k, '-share-footer.png" alt="', strtoupper($k), '"/></a></li>';
+            }
+            ?>
+        </ul>
+    </div>
+</footer><!-- #colophon -->
 </div><!-- #page -->
 <?php wp_footer(); ?>
 </body>
