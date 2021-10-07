bf30bd0baaafa3a1ececbac9bc41d52c27aff5fe
Paulo <vieirajjr@gmail.com>
2021-10-07 15:17:07 -0300

    Correção texto introdução TSE_painel_Trajeotria

--- wp-content/plugins/trajetoria-escolar/src/FrontEnd.php
+++ wp-content/plugins/trajetoria-escolar/src/FrontEnd.php
@@ -873,7 +873,17 @@ class FrontEnd
                             <div id="painel_trajetorias">
 
                                 <p>
-                                Esse painel apresenta simulações de trajetórias educacionais entre 2015 a 2019 para três segmentos: Matriculas de 6 anos no 1º ano do Ensino Fundamental em 2015, Matrículas de 10 anos no 5º ano do Ensino Fundamental em 2015 e Matriculas de 14 anos no 9º ano do Ensino Fundamental em 2016. Para cada um desses segmentos acompanhou-se as matriculas nos anos seguintes segundo a idade. Por exemplo, para o primeiro segmento acompanhou-se as matriculas de 7 anos no 2º ano do Ensino Fundamental em 2016 e assim sucessivamente até as matriculas de 10 anos no 5º ano do Ensino Fundamental. O mesmo procedimento foi adotado para os outros segmentos. Dessa forma é possível, para a região analisada, acompanhar o processo de sucesso e de exclusão da escola a cada ano, indicando os casos mais críticos e que necessitam de ações visando garantir o direito de todos e todas à educação.
+                                
+                                <p>Esse painel apresenta simulações de trajetórias educacionais entre 2015 até 2020 para quatro segmentos:
+                                <br><li>Coorte de matrículas de 6 anos no 1º ano do Ensino Fundamental em 2015;</li>	
+                                <br><li>Coorte de matrículas de 10 anos no 5º ano do Ensino Fundamental em 2015;</li>
+                                <br><li>Coorte de matrículas de 14 anos no 9º ano do Ensino Fundamental em 2016;</li>
+                                <br><li>Coorte de matrículas de 14 anos no 9º ano do Ensino Fundamental em 2017.</li>
+                                <p>Para cada um desses segmentos acompanhou-se as matrículas nos anos seguintes, segundo a idade, por exemplo: para o primeiro segmento, acompanhou-se as matrículas de 7 anos no 2º ano do Ensino Fundamental em 2016, e assim sucessivamente até as matriculas de 11 anos no 6º ano do Ensino Fundamental em 2020. 
+                                <br>O mesmo procedimento foi adotado para os demais segmentos.</p>
+                                <p>Dessa forma é possível, para a região analisada, acompanhar o processo de sucesso e de exclusão da escola a cada ano, indicando os casos mais críticos e que necessitam de ações visando garantir o direito de todos e todas à educação.</P>
+
+                            
                                 </p>
 
                                 <!-- <div id="seletores">
